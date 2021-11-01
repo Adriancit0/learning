052618e2122289fb1400e98151377eaae46a98c1
Adriancit0 <adrianruanol@gmail.com>
2021-11-01 20:35:06 +0100

    Add style and install wouter

--- consumo-api/src/App.jsx
+++ consumo-api/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Route, Link } from 'wouter';
 import ListOfGifs from './components/ListOfGifs';
 
 function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs keyword="mapache" />
+        <h1>App</h1>
+        <Link to="/gif/pandas">Gifs de pandas</Link>
+        <Link to="/gif/mapaches">Gifs de mapaches</Link>
+        <Link to="/gif/patos">Gifs de patos</Link>
+        <Link to="/gif/panellets">Gifs de panellets</Link>
+        <Route
+          component={ListOfGifs}
+          path="/gif/:keyword"
+        />
       </section>
     </div>
   );
